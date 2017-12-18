0d1d64585e1334df41093e53d99bd6dff93a1f02
BartoszDabek <bartek50600@wp.pl>
2017-12-18 10:56:49 +0100

    change implementation of getting current location. Now it's getting by google api service. Also added full support for permission requesting on devices with android >=6.0 and < 6.0

---
+++ app/src/main/java/bdabek/com/simplefinder/Constatns.kt
@@ -0,0 +1,11 @@
+package bdabek.com.simplefinder
+
+import android.Manifest
+
+/**
+ * Created by Bartek on 2017-12-18.
+ */
+val STARTING_DISTANCE = 3f
+val REQUEST_ID_MULTIPLE_PERMISSIONS = 1
+val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION)
--- app/src/main/java/bdabek/com/simplefinder/MainActivity.kt
+++ app/src/main/java/bdabek/com/simplefinder/MainActivity.kt
@@ -1,11 +1,8 @@
 package bdabek.com.simplefinder
 
 import android.annotation.SuppressLint
-import android.content.Context
+import android.content.IntentSender
 import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
@@ -14,19 +11,19 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.SeekBar
 import android.widget.Toast
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    var hasAuthorization: Boolean = false
-    var locationManager: LocationManager? = null
-    var locationListener: LocationListener? = null
+    private var mFusedLocationClient: FusedLocationProviderClient? = null
+    private var mLocationCallback: LocationCallback? = null
 
-    var distance: Float = 3f
+    var distance: Float = STARTING_DISTANCE
     var longtitude: Double? = null
     var latitude: Double? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,19 +31,85 @@ class MainActivity : AppCompatActivity() {
 
         distanceTxt.text = "$distance km"
 
-        checkPermissions()
+        if (checkPermissions()) {
+            createLocationRequest()
+        }
+        // because user accepted permissions while download from app store
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            createLocationRequest()
+        }
 
         findBtn.setOnClickListener {
-            if (hasAuthorization && (latitude !== null && longtitude !== null)) {
-                Log.d("Distance In Meters", "${(distance * 1000).toInt()}")
-                Log.d("Current Longtitude", "$longtitude")
-                Log.d("Current Latitude", "$latitude")
-                msgTxt.append("CURRENT LATTITUDE: $latitude \n")
-            } else {
-                askForPermissions()
+            if(checkPermissions()) {
+                Log.d("LONGTITUDE: ", "$longtitude")
+                Log.d("LATITUDE: ", "$latitude")
             }
         }
 
+        mLocationCallback = MyLocationCallback()
+        addDistanceSeekBarListener()
+    }
+
+    private fun checkPermissions(): Boolean {
+        var result: Int
+        val listPermissionsNeeded = ArrayList<String>()
+        for (p in permissions) {
+            result = ContextCompat.checkSelfPermission(this, p)
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p)
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toTypedArray(), REQUEST_ID_MULTIPLE_PERMISSIONS)
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_ID_MULTIPLE_PERMISSIONS -> {
+                // grantResults[0] returns permission only for ACCESS_FINE_LOCATION!
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    createLocationRequest()
+                } else {
+                    Toast.makeText(this, "The app will not work without these permissions!", Toast.LENGTH_LONG)
+                            .show()
+                }
+                return
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun createLocationRequest() {
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.interval = 10000
+        mLocationRequest.fastestInterval = 5000
+        mLocationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+
+        var builder = LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest)
+
+        val client = LocationServices.getSettingsClient(this)
+        val task = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener(this) {
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+            mFusedLocationClient?.requestLocationUpdates(mLocationRequest, mLocationCallback, null)
+        }
+
+        task.addOnFailureListener(this) { e ->
+            if (e is ResolvableApiException) {
+                try {
+                    e.startResolutionForResult(this@MainActivity, REQUEST_ID_MULTIPLE_PERMISSIONS)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                }
+
+            }
+        }
+    }
+
+    private fun addDistanceSeekBarListener() {
         distanceBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 Log.d("Distance", "${progress.div(2f)}")
@@ -62,54 +125,14 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun checkPermissions() {
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            askForPermissions()
-        } else {
-            getLocation()
-        }
-    }
-
-    private fun askForPermissions() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION,
-                    android.Manifest.permission.ACCESS_COARSE_LOCATION),
-                    10)
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun getLocation() {
-        hasAuthorization = true
-
-        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        locationListener = MyLocationListener()
-        locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10f, locationListener)
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        when (requestCode) {
-            10 -> if (grantResults[0] == 0) getLocation()
-        }
-    }
-
-
-    inner class MyLocationListener : LocationListener {
-        override fun onLocationChanged(location: Location) {
-            Log.d("Current Longtitude", "${location.longitude}")
-            Log.d("Current Latitude", "${location.latitude}")
-            longtitude = location.longitude
-            latitude = location.latitude
+    inner class MyLocationCallback : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            for (location in locationResult.locations) {
+                longtitude = location.longitude
+                latitude = location.latitude
+            }
         }
 
-        override fun onProviderDisabled(provider: String) {
-            Toast.makeText(this@MainActivity, "GPS is OFF!", Toast.LENGTH_SHORT).show()
-        }
-
-        override fun onProviderEnabled(provider: String) {}
-
-        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
     }
 
 }
